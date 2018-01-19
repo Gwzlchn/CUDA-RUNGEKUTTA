8dc363052a68173dc36678c846bb5888ea2a4a40
Zelin <wangzl1116@mails.jlu.edu.cn>
2018-01-20 01:01:21 +0800

    ThreeDim

---
+++ ThreeDim/nucleus.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef NUCLEUS_HPP
+#define	NUCLEUS_HPP
+
+
+
+struct nucleus{
+	double x;
+	double y;
+	double z;
+	
+	double px;
+	double py;
+	double pz;
+};
+
+struct  nuclei{
+	nucleus first;
+	nucleus second;
+};
+
+
+
+
+
+
+#endif //NUCLEUS_HPP
