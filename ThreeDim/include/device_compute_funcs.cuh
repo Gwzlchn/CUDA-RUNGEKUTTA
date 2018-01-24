184f58bda4172db79b18ec83fb4e291eea6e82bb
Zelin <wangzl1116@mails.jlu.edu.cn>
2018-01-24 11:36:22 +0800

    fx

--- ThreeDim/include/device_compute_funcs.cuh
+++ ThreeDim/include/device_compute_funcs.cuh
@@ -1,4 +1,4 @@
-#ifndef DEVICE_COMPUTE_FUNCS_CUH
+﻿#ifndef DEVICE_COMPUTE_FUNCS_CUH
 #define DEVICE_COMPUTE_FUNCS_CUH
 
 #include "sci_const.cuh"
@@ -7,18 +7,60 @@
 #include <thrust/device_vector.h>
 
 
+//两核之间距离的平方
+//返回（x1-x2)^2 +（y1-y2)^2 +（z1-z2)^2
+__device__  double nucleus_distance(const nucleus& first, const nucleus& second);
 
+//第一个核，三个坐标的二阶导
+__device__ nucleus fx_first_nucleus(const nucleus& first, const nucleus& second);
+//第二个核，三个坐标的二阶导
+__device__ nucleus fx_second_nucleus(const nucleus& first, const nucleus& second);
 
-__device__ double fx_first(const nucleus& first, const nucleus& second,double t,double a,double a1 );
 __device__ void update_step_one(nucleus* step_one_fir, nucleus* step_one_sec);
 __device__ void update_step_two(nucleus* step_two_fir, nucleus* step_two_sec);
 
 #endif //DEVICE_COMPUTE_FUNCS_CUH
 
-__device__  double fx_first(const nucleus& first, const nucleus& second, double t,double a,double a1)
+
+
+
+
+__device__  double nucleus_distance(const nucleus& first, const nucleus& second)
 {
-	return (first.x - second.x) / sqrt(pow((pow((first.x - second.x), 2) + pow((first.y - second.y), 2) + pow((first.z - second.z), 2) + a1*a1), 3)) -
-		(first.x - nuclear_spacing / 2.0*sin(PI*rotation)) / sqrt((pow((first.z - nuclear_spacing / 2.0*cos(PI*rotation)), 2)));
+	return (pow((first.x - second.x), 2) + pow((first.y - second.y), 2) + pow((first.z - second.z), 2));
+}
+
+
+
+
+__device__  nucleus fx_first_nucleus(const nucleus& first, const nucleus& second)
+{
+	nucleus fx_first;
+	fx_first.x = (first.x - second.x)
+			/ sqrt(pow((nucleus_distance(first, second) + elec_nucl*elec_nucl), 3)) -
+		(first.x - nuclear_spacing / 2.0 * sin(PI*rotation))
+			/ sqrt(pow((pow((first.z - nuclear_spacing / 2.0*cos(PI*rotation)), 2) + pow((first.x - nuclear_spacing / 2.0 * sin(PI*rotation)), 2) + first.y*first.y + elec_elec*elec_elec), 3)) -
+		(first.x + nuclear_spacing / 2.0 * sin(PI*rotation))
+			/ sqrt(pow((pow((first.z + nuclear_spacing / 2.0*cos(PI*rotation)), 2) + pow((first.x + nuclear_spacing / 2.0 * sin(PI*rotation)), 2) + first.y*first.y + elec_elec*elec_elec), 3));
+	fx_first.y = (first.y-second.y)/sqrt(pow((nucleus_distance(first,second)+elec_nucl*elec_nucl),3)-
+			first.y/sqrt(pow((first.z-nuclear_spacing/2.0*cos(PI*rotation)),2)-pow((first.x-nuclear_spacing/2.0*sin(PI*rotation)),2)+))
+
+
+
+	return fx_first;
+}
+__device__  nucleus fx_second_nucleus(const nucleus& first, const nucleus& second)
+{
+	nucleus fx_second;
+	fx_second.x = (second.x - first.x) 
+				/ sqrt(pow((nucleus_distance(first, second) + elec_nucl*elec_nucl), 3)) -
+		(second.x - nuclear_spacing / 2.0 * sin(PI*rotation))
+				/ sqrt(pow((pow((second.z - nuclear_spacing / 2.0*cos(PI*rotation)), 2) + pow((second.x - nuclear_spacing / 2.0 * sin(PI*rotation)), 2) + second.y*second.y + elec_elec*elec_elec), 3)) -
+		(second.x + nuclear_spacing / 2.0 * sin(PI*rotation))
+		/ sqrt(pow((pow((second.z + nuclear_spacing / 2.0*cos(PI*rotation)), 2) + pow((second.x + nuclear_spacing / 2.0 * sin(PI*rotation)), 2) + second.y*second.y + elec_elec*elec_elec), 3));
+
+
+	return fx_second;
 }
 
 
--- ThreeDim/include/sci_const.cuh
+++ ThreeDim/include/sci_const.cuh
@@ -11,6 +11,8 @@ __global__ double nuclear_spacing = 4.0;//核间距,对应之前的R
 
 __global__ double PI = 3.1415926535897932384626433832795;//圆周率
 
+__global__ double elec_nucl = 0.1;//电子与核之间参数，对应之前A1
 
+__global__ double elec_elec = 1.25;//电子与电子之间参数。对应之前A
 
 #endif
