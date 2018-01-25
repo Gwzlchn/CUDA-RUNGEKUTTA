05ddb6137acce43d70a29b3a0215d8cd5bd5ca0b
Zelin <wangzl1116@mails.jlu.edu.cn>
2018-01-25 23:58:10 +0800

    255错误

--- ThreeDim/main.cpp
+++ ThreeDim/main.cpp
@@ -17,7 +17,7 @@
 ////用于双核粒子的随机数化
 ////参数:	Array:粒子数组	Size:数组长度	Angle:偏移角(0)
 //extern "C" void NucleiRandomD(nuclei* Array, const long Size, double Angle = 0);
-
+#pragma warning(disable:C4819)
 int main()
 {
 	double rotation = 0.0;//转轴角度，对应之前的kk
